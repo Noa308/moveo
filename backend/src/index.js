8483f37a42a929e0bf24ac7df4e78e46681e8c35
Noa Levy <noa308levy@gmail.com>
2025-01-01 21:42:54 +0200

    test

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,92 +36,92 @@ let state = {
 };
 app.listen(3001);
 
-server.listen(3000, async () => {
-  //init
-  // await connect();
-  const { rows } = await client.query(`select * from code_blocks`);
-  state["codeBlocks"] = rows;
+// server.listen(3000, async () => {
+//   //init
+//   await connect();
+//   const { rows } = await client.query(`select * from code_blocks`);
+//   state["codeBlocks"] = rows;
 
-  //allow connections
-  io.on("connection", (socket) => {
-    //"connection" is a saved word
-    console.log("new connection");
-    const userId = socket.id;
+//   //allow connections
+//   io.on("connection", (socket) => {
+//     //"connection" is a saved word
+//     console.log("new connection");
+//     const userId = socket.id;
 
-    let toSend = {
-      codeBlocks: state["codeBlocks"],
-      activeCodeBlockId: state["activeCodeBlockId"],
-    };
-    socket.emit("codeBlockInfo", toSend);
-    console.log("send the coode blocks info");
+//     let toSend = {
+//       codeBlocks: state["codeBlocks"],
+//       activeCodeBlockId: state["activeCodeBlockId"],
+//     };
+//     socket.emit("codeBlockInfo", toSend);
+//     console.log("send the coode blocks info");
 
-    socket.on("enterCodeBlock", (codeBlockId) => {
-      console.log(`enter code block: ${codeBlockId} the user: ${userId}`);
+//     socket.on("enterCodeBlock", (codeBlockId) => {
+//       console.log(`enter code block: ${codeBlockId} the user: ${userId}`);
 
-      //check if Tom enter the room/ other student
-      if (state["activeCodeBlockId"] !== -1) {
-        if (!state["activeUsers"].has(userId)) {
-          state["userCount"]++;
-          console.log(`student enter the block id , student:${userId}`);
-          io.emit("number of users", state["userCount"]);
-          state["activeUsers"].set(userId, userId);
-        }
-      } else {
-        state["activeCodeBlockId"] = codeBlockId;
-        io.emit("codeBlockActivated", codeBlockId);
-        if (!state["activeUsers"].has(userId)) {
-          state["activeUsers"].set(userId, userId);
-        }
-        socket.emit("this is the mentor");
-        state["mentorId"] = userId;
-        console.log(`mentor enter the code block, id: ${userId}`);
-      }
-    });
+//       //check if Tom enter the room/ other student
+//       if (state["activeCodeBlockId"] !== -1) {
+//         if (!state["activeUsers"].has(userId)) {
+//           state["userCount"]++;
+//           console.log(`student enter the block id , student:${userId}`);
+//           io.emit("number of users", state["userCount"]);
+//           state["activeUsers"].set(userId, userId);
+//         }
+//       } else {
+//         state["activeCodeBlockId"] = codeBlockId;
+//         io.emit("codeBlockActivated", codeBlockId);
+//         if (!state["activeUsers"].has(userId)) {
+//           state["activeUsers"].set(userId, userId);
+//         }
+//         socket.emit("this is the mentor");
+//         state["mentorId"] = userId;
+//         console.log(`mentor enter the code block, id: ${userId}`);
+//       }
+//     });
 
-    socket.on("changeCode", (codeToShow) => {
-      state["editedCode"] = codeToShow;
-      io.emit("editedCode", codeToShow);
-      console.log("edit code");
-    });
+//     socket.on("changeCode", (codeToShow) => {
+//       state["editedCode"] = codeToShow;
+//       io.emit("editedCode", codeToShow);
+//       console.log("edit code");
+//     });
 
-    socket.on("changeTextDisplay", (bool) => {
-      state["showText"] = bool;
-      io.emit("showText", bool);
-      console.log("change the show", bool);
-    });
-    //"on" is when this event happend
-    //"emit" send the event (io.emit = send to all connected sockets, socket.emit = send to this socket)
+//     socket.on("changeTextDisplay", (bool) => {
+//       state["showText"] = bool;
+//       io.emit("showText", bool);
+//       console.log("change the show", bool);
+//     });
+//     //"on" is when this event happend
+//     //"emit" send the event (io.emit = send to all connected sockets, socket.emit = send to this socket)
 
-    let userLeft = () => {
-      console.log(`user disconnected, user: ${userId}`);
-      state["activeUsers"].delete(userId);
-      state["userCount"]--;
-      io.emit("number of users", state["userCount"]);
-    };
+//     let userLeft = () => {
+//       console.log(`user disconnected, user: ${userId}`);
+//       state["activeUsers"].delete(userId);
+//       state["userCount"]--;
+//       io.emit("number of users", state["userCount"]);
+//     };
 
-    socket.on("user left the code block", userLeft);
+//     socket.on("user left the code block", userLeft);
 
-    let mentorLeft = () => {
-      console.log(`mentor disconnected, mentor: ${userId}`);
-      state = {
-        ...state,
-        ...defaultState,
-      };
-      io.emit("restart code block id");
-    };
-    socket.on("mentor left the code block", mentorLeft);
-    socket.on("disconnect", () => {
-      if (userId === state["mentorId"]) {
-        mentorLeft();
-      } else {
-        userLeft();
-      }
-    });
-  });
-});
+//     let mentorLeft = () => {
+//       console.log(`mentor disconnected, mentor: ${userId}`);
+//       state = {
+//         ...state,
+//         ...defaultState,
+//       };
+//       io.emit("restart code block id");
+//     };
+//     socket.on("mentor left the code block", mentorLeft);
+//     socket.on("disconnect", () => {
+//       if (userId === state["mentorId"]) {
+//         mentorLeft();
+//       } else {
+//         userLeft();
+//       }
+//     });
+//   });
+// });
 
 async function cleanup() {
-  await client.end();
+  // await client.end();
   setTimeout(function () {
     console.error("Could not close connections in time, forcing shut down");
     process.exit(1);
